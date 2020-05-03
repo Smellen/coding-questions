cbfb278ddbe5a57def0099e89e8c321502308e68
Smellen <eburke6@tcd.ie>
2020-05-03 18:47:14 +0100

    Adding inital isUnique solution

---
+++ strings-and-lists/is_unique.py
@@ -0,0 +1,16 @@
+def is_unique(word):
+    print('Checking if \'' + word + '\' contains unique characters. ')
+    found = []
+    for letter in word:
+        if letter not in found:
+            found.append(letter)
+        else:
+            print('The letter ' + letter + ' is not unique.')
+            return
+
+    print('The word \'' + word + '\' is unique')
+
+
+is_unique('ellen')
+is_unique('abcdefg')
+is_unique('Adm Jenso')
